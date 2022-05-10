caabde6b7f5d0af6848a602dcbdad6f4da95f08c
“Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-05-10 14:59:49 +0200

    Initial Commit

---
+++ swiftuiweather/swiftuiweatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiweatherApp.swift
+//  swiftuiweather
+//
+//  Created by Cloud Secrets on 10/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiweatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
